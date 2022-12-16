6b9fa0972a9c381d4818a86cc42c2e1956727325
yeonuk0401 <yeonuk44@naver.com>
2022-12-16 20:48:09 +0900

    react-hook-form 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 
-function App() {
+import { useForm } from 'react-hook-form';
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  // console.log(watch('example'));
   return (
-    <>
-      <p>asdasds</p>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="test" {...register('example')} />
+      <input {...register('exampleRequired', { required: true })} />
+      <select {...register('gender')}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      {errors.exampleRequired && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
